887d5fbb63fc43c00af99db7ab4d9d466956aa12
Austen <e0407786u.nus.edu>
2020-08-17 00:32:13 +0800

    Updates

---
+++ precache-manifest.4ba800ff878fb283f5629c497d5d88a0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f260ce8cab09046104810ad10644036e",
+    "url": "/goagetcap5/index.html"
+  },
+  {
+    "revision": "3997a124180f569d7f01",
+    "url": "/goagetcap5/static/css/main.cc73a5e4.chunk.css"
+  },
+  {
+    "revision": "af1005f0352d1aca5bb7",
+    "url": "/goagetcap5/static/js/2.396c380f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goagetcap5/static/js/2.396c380f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3997a124180f569d7f01",
+    "url": "/goagetcap5/static/js/main.1825424b.chunk.js"
+  },
+  {
+    "revision": "043aaec1acefa103a585",
+    "url": "/goagetcap5/static/js/runtime-main.43518c8b.js"
+  },
+  {
+    "revision": "3056af40268389bbc8b1fc191162861e",
+    "url": "/goagetcap5/static/media/07xo8frt.3056af40.bmp"
+  },
+  {
+    "revision": "65f58b7a93be2c5839bba92dd9403b48",
+    "url": "/goagetcap5/static/media/0i5lxabv.65f58b7a.bmp"
+  },
+  {
+    "revision": "1d4955507e019e99a1f2e7c6fdd65278",
+    "url": "/goagetcap5/static/media/133312.1d495550.jpg"
+  },
+  {
+    "revision": "d570ebbc9361289177f227b4d4900ff0",
+    "url": "/goagetcap5/static/media/21765274_1844492998901820_7157605741638401878_n1.d570ebbc.jpg"
+  },
+  {
+    "revision": "df3fdfcb264c92f03f4e69994a654fba",
+    "url": "/goagetcap5/static/media/25kwi7zr.df3fdfcb.bmp"
+  },
+  {
+    "revision": "890c0aec6f963b0ca777cf004cd95f71",
+    "url": "/goagetcap5/static/media/37kdrwuv.890c0aec.bmp"
+  },
+  {
+    "revision": "2ca11c7cfb9db99ae5b477f61c3cdf3b",
+    "url": "/goagetcap5/static/media/95rot1ix.2ca11c7c.bmp"
+  },
+  {
+    "revision": "58539a629c7c2dd54d57609de4bf4598",
+    "url": "/goagetcap5/static/media/ej9tbhei.58539a62.bmp"
+  },
+  {
+    "revision": "31c952780ffd390feb7e9850b5f6cf68",
+    "url": "/goagetcap5/static/media/goa.31c95278.jpg"
+  },
+  {
+    "revision": "d7d3b1e3a64c834ab49890ba2d925e7f",
+    "url": "/goagetcap5/static/media/ngib4nez.d7d3b1e3.bmp"
+  },
+  {
+    "revision": "d77bf4718b27b5c748a971cdcb8ce324",
+    "url": "/goagetcap5/static/media/qliz96kw.d77bf471.bmp"
+  },
+  {
+    "revision": "37bc301a2de01a4613f9453b8748ac7a",
+    "url": "/goagetcap5/static/media/v7jh4j4f.37bc301a.bmp"
+  },
+  {
+    "revision": "8ff7ba9afe4b3f4ea8738cec611fd601",
+    "url": "/goagetcap5/static/media/vdmst7xs.8ff7ba9a.bmp"
+  },
+  {
+    "revision": "a9c88880a2f3ed88a19d3d47d8a47293",
+    "url": "/goagetcap5/static/media/x7if2fin.a9c88880.bmp"
+  },
+  {
+    "revision": "66550120d7f6ecf09a4e3c3b2a4afe5e",
+    "url": "/goagetcap5/static/media/zwvfovcg.66550120.bmp"
+  }
+]);
